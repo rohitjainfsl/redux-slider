e02ce340e314a97c8d4547e5fda7a3c2aa6e5fd4
rohitjainfsl <rohit@fullstacklearning.com>
2023-09-08 09:49:11 +0530

    changes in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
-import "./App.css"
+import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getData, incrementSlider, decrementSlider } from "./slice";
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 function App() {
   const dispatch = useDispatch();
@@ -9,47 +12,46 @@ function App() {
     return state.slider;
   });
 
-  
   useEffect(() => {
     dispatch(getData(init.page));
   }, [init.page]);
 
-  if (!init.isLoading) return <p>Loading...</p>;
-  else {
-    return (
-      <>
+  return (
+    <>
       <div className="maindiv">
-
-        <div className="slider">
-          <div className="slider-wrapper">
-            <div className="left">
-              <img src={init.product.image} alt="Product Image" />
-            </div>
-            <div className="right">
-              <h2>{init.product.title}</h2>
-              <p>{init.product.description}</p>
-              <p>{init.product.price}</p>
-            </div>
-          </div>
-          <div className="navigation">
-            <button
-              onClick={() => dispatch(decrementSlider())}
-              disabled={init.page === 1 ? true : false}
-              >
-              Prev
-            </button>
-            <button
-              onClick={() => dispatch(incrementSlider())}
-              disabled={init.page === 19 ? true : false}
-              >
-              Next
-            </button>
-          </div>
+        {!init.isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="slider">
+            <div className="slider-wrapper">
+              <div className="left">
+                <img src={init.product.image} alt="Product Image" />
               </div>
-        </div>
-      </>
-    );
-  }
+              <div className="right">
+                <h2>{init.product.title}</h2>
+                <p>{init.product.description}</p>
+                <p className="pricing"><AttachMoneyIcon />{init.product.price}</p>
+              </div>
+            </div>
+            <div className="navigation">
+              <button
+                onClick={() => dispatch(decrementSlider())}
+                disabled={init.page === 1 ? true : false}
+              >
+                <ArrowLeftIcon />
+              </button>
+              <button
+                onClick={() => dispatch(incrementSlider())}
+                disabled={init.page === 19 ? true : false}
+              >
+                <ArrowRightIcon />
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
